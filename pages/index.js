a03af2ba4493302e3bd4bdbbd9bbc53ef7c1c153
alexeiaccio <alexei.accio@gmail.com>
2021-07-08 04:54:43 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ pages/index.js
@@ -0,0 +1,26 @@
+import Hero from "../sections/Hero";
+import Features from "../sections/Features";
+import HowItWorks from "../sections/HowItWorks";
+import Faq from "../sections/FAQ";
+import UserBrands from "../sections/UserBrands";
+import Testimonials from "../sections/Testimonials";
+import EmailCapture from "../sections/EmailCapture";
+import Layout from '../components/Layout'
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <HowItWorks />
+      <Features />
+      <Faq />
+      <UserBrands />
+      <Testimonials />
+      <EmailCapture />
+    </>
+  );
+}
+
+Home.getLayout = (page) => (
+  <Layout pageName="Home">{page}</Layout>
+);
